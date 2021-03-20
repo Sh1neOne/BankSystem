584901761ee3e4850fafa86268e1a10de7b3e85d
Alexey Salamakhin <salamakhin@list.ru>
2021-03-20 17:35:09 +0300

    Добавьте файлы проекта.

---
+++ BankSystem/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem
+{
+    class Client
+    {
+        private string firstName;
+        private string lastName;
+        private string id;
+
+        public Client(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Id = "";
+        }
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string Id { get => id; set => id = String.IsNullOrEmpty(value)?Guid.NewGuid().ToString().Substring(0,5):value; }
+    }
+}
